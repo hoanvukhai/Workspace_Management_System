0388001cbb2e32f5b39a6e637ef830ecb3893c68
Hoan <balaminmau@gmail.com>
2026-01-31 18:59:45 +0700

    Add Vercel URL to CORS

--- task-management/backend/server.js
+++ task-management/backend/server.js
@@ -5,7 +5,22 @@ const db = require("./config/db");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:3000",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
